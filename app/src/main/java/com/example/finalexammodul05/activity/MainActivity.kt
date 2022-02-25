334a7e72a88d0ecfadecc023cbb8adee74601c6a
Mardiyev Abbos <abbosmardiev1999@gmail.com>
2022-02-25 14:20:01 +0500

    Initial commit

---
+++ app/src/main/java/com/example/finalexammodul05/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.finalexammodul05.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalexammodul05.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
